4827dcc54fe9f81b91493d19ed8c48b3f83e40f7
Sudip <sudipdasghosh64@gmail.com>
2024-07-16 18:43:30 +0530

    done

--- script.js
+++ script.js
@@ -3,49 +3,49 @@ var arr = [
     songName: "Sajni re",
     url: "./song/Sajni.mp3",
     img: "./img/SAJNI.jpg",
-    time: "3:00"
+    time: "3:00",
   },
   {
     songName: "Pehli bhi mein",
     url: "./song/Pehle Bhi Main.mp3",
     img: "./img/ANIMAL.jpg",
-    time: "3:10"
+    time: "3:10",
   },
   {
     songName: "Ki Honda Pyaar",
     url: "./song/Ki Honda Pyaar.mp3",
     img: "./img/Ki-Honda-Pyar.jpg",
-    time: "2:01"
+    time: "2:01",
   },
   {
     songName: "Mehrama",
     url: "./song/Mehrama.mp3",
     img: "./img/Mehrama.jpg",
-    time: "4:10"
+    time: "4:10",
   },
   {
     songName: "Jeena Sikha De",
     url: "./song/JEENA_SIKHA_DE.mp3",
     img: "./img/Srikanth.jpg",
-    time: "2:10"
+    time: "2:10",
   },
   {
     songName: "DARMIYAAN",
     url: "./song/DARMIYAAN.mp3",
     img: "./img/DARMIYAAN.jpg",
-    time: "2:10"
+    time: "2:10",
   },
   {
     songName: "Mujhe Kaise, Pata Na Chala",
     url: "./song/Mujhe_Kaise,_Pata_Na_Chala.mp3",
     img: "./img/Mujhe_Kaise,_Pata_Na_Chala.jpg",
-    time: "2:10"
+    time: "2:10",
   },
   {
     songName: "Kuch To Hai",
     url: "./song/Kuch_To_Hai.mp3",
     img: "./img/Kuch_To_Hai.jpg",
-    time: "2:10"
+    time: "2:10",
   },
 ];
 
@@ -54,9 +54,6 @@ var poster = document.querySelector(".song-banner");
 var play = document.querySelector("#play");
 var backward = document.querySelector("#backward");
 var forward = document.querySelector("#forward");
-
-
-
 
 var audio = new Audio();
 
@@ -83,68 +80,63 @@ function mainFunction() {
 mainFunction();
 
 songList.addEventListener("click", function (dets) {
-  selectedSong = dets.target.id
-  play.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`
+  selectedSong = dets.target.id;
+  play.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`;
   mainFunction();
   audio.play();
 });
 
 var flag = 0;
-play.addEventListener("click", function(){
-    if(flag == 0){
-        play.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`
-        mainFunction()
-        audio.play()
-        flag = 1
-    } else {
-        play.innerHTML = `<i class="fa-solid fa-circle-play"></i>`
-        mainFunction()
-        audio.pause()
-        flag = 0
-    }
-})
-
-forward.addEventListener("click", function(){
-    if(selectedSong < arr.length - 1){
-        selectedSong++
-        play.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`
-        mainFunction()
-        audio.play()
-    } else{
-        forward.style.opacity = 0.3;
-        audio.pause()
-    }
-})
-
-backward.addEventListener("click", function(){
-    if(selectedSong > 0){
-        selectedSong--
-        play.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`
-        mainFunction()
-        audio.play()
-    } else{
-        backward.style.opacity = 0.3;
-        audio.pause()
-    }
-})
-
-audio.addEventListener("ended", function(){
-  if(selectedSong < arr.length - 1){
-      selectedSong++
-      play.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`
-      mainFunction()
-      audio.play()
-  } else{
-      forward.style.opacity = 0.3;
-      audio.pause()
+play.addEventListener("click", function () {
+  if (flag == 0) {
+    play.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`;
+    mainFunction();
+    audio.play();
+    flag = 1;
+  } else {
+    play.innerHTML = `<i class="fa-solid fa-circle-play"></i>`;
+    mainFunction();
+    audio.pause();
+    flag = 0;
   }
-})
-// function playNextSong() {
-//   selectedSong = (selectedSong + 1) % arr.length;
-//   audio.src = arr[selectedSong].url;
-//   audio.load();
-//   audio.play();
-//   mainFunction();
-// }
+});
 
-// audio.addEventListener('ended', playNextSong);
+forward.addEventListener("click", function () {
+  if (selectedSong < arr.length - 1) {
+    selectedSong++;
+    play.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`;
+    mainFunction();
+    audio.play();
+    backward.style.opacity = 100;
+  } else {
+    play.innerHTML = `<i class="fa-solid fa-circle-play"></i>`;
+    forward.style.opacity = 0.3;
+    audio.pause();
+  }
+});
+
+backward.addEventListener("click", function () {
+  if (selectedSong > 0) {
+    selectedSong--;
+    play.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`;
+    mainFunction();
+    audio.play();
+    forward.style.opacity = 100;
+  } else {
+    play.innerHTML = `<i class="fa-solid fa-circle-play"></i>`;
+    backward.style.opacity = 0.3;
+    audio.pause();
+  }
+});
+
+audio.addEventListener("ended", function () {
+  if (selectedSong < arr.length - 1) {
+    selectedSong++;
+    play.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`;
+    mainFunction();
+    audio.play();
+  } else {
+    forward.style.opacity = 0.3;
+    audio.pause();
+  }
+});
